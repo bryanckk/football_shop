5ac5c66a6ae669ceef0edef3fea2f0ba7c8d06b9
bryanckk <bryan.ck1612@gmail.com>
2025-09-17 02:40:06 +0700

    Progress Tanpa Readme

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -34,7 +34,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bryan-christopher-footballshop.pbp.cs.ui.ac.id"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/web/project/bryan.christopher/footballshop"
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -62,7 +64,7 @@ ROOT_URLCONF = 'football_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
