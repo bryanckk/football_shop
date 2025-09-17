01050107bac0071581b88f41cdd1f523f6a4d894
bryanckk <bryan.ck1612@gmail.com>
2025-09-17 11:42:45 +0700

    Tugas 3 Final Amin Banget

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bryan-christopher-footballshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://pbp.cs.ui.ac.id/web/project/bryan.christopher/footballshop"
+    "https://bryan-christopher-footballshop.pbp.cs.ui.ac.id/"
 ]
 # Application definition
 
